da1e88795c577d9abdb43d27102bea8eb1a262a1
PersianXM <ali.rashedi@gmail.com>
2024-01-31 12:07:45 +0330

    First Commit

---
+++ Home.py
@@ -0,0 +1,40 @@
+# Syntax
+# Physical Lines
+# Logical Lines
+
+print("Hello World")
+print("Hello World 1")
+print("Hello World 2")
+
+x = 7
+print(9)
+
+y = (7 + 8 + 7
+     + 8 + 7 + 8)
+
+print(x)
+
+# Comment
+# DocString
+
+# برای گرفتن ورودی از کاربر
+# To get input
+x = input("x: ")  # To get input
+y = input("y: ")  # To get input
+
+
+# Ctrl + /
+# Ctrl + Shift + /
+
+# this is sample text. this is sample text.
+# this is sample text. this is sample text.
+# this is sample text. this is sample text.
+#
+# this is sample text. this is sample text.
+# this is sample text. this is sample text.
+# this is sample text. this is sample text.
+
+def F():
+    """for printing my name"""
+    print("Reza")
+
