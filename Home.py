5af5ca7c814e22ee453a1b08d7fea874b707172c
PersianXM <ali.rashedi@gmail.com>
2024-02-02 16:20:37 +0330

    input and output new file

--- Home.py
+++ Home.py
@@ -1,3 +1,72 @@
+a = 2
+print(a)
+b = a + 2
+
+# this code will print b
+
+print(b)
+
+c = 3.14
+print(c)
+
+x = float(b)
+print(x)
+
+y = int(c)
+print(y)
+
+list1 = [1, 2, 3, 4, 5]
+for i in list1:
+    print(i)
+    if i == 4:
+        break
+# Important of Indentation
+print("End of for loop")
+
+number = 10
+print(number)
+
+_a = 200
+print(_a)
+
+x_y = 2000
+print(x_y)
+print(x_y)
+print(x_y)
+print(x_y)
+
+# Ctrl + Alt + L
+# Ctrl + D
+# Ctrl + W
+# Ctrl + /
+# Alt + Space
+# Ctrl + Shift + I (Quick Definition)
+# Ctrl + Q (Quick Document)
+
+# Alt + Enter (Find and Import Needed library's)
+from cmath import sin
+
+sin(25)
+
+
+# Ctrl + alt + O (Delete Unwanted library's)
+# import random
+# import posix
+
+
+def f():
+    def b():
+        print()
+        print()
+        print()
+        print()
+        print()
+
+
+# ctrl + Z
+# ctrl + shift + Z
+
+
 # Syntax
 # Physical Lines
 # Logical Lines
@@ -17,7 +86,7 @@ print(x)
 # Comment
 # DocString
 
-# برای گرفتن ورودی از کاربر
+# to get input from user
 # To get input
 x = input("x: ")  # To get input
 y = input("y: ")  # To get input
@@ -35,7 +104,17 @@ y = input("y: ")  # To get input
 # this is sample text. this is sample text.
 
 
-def F():
+def f():
     """for printing my name"""
     print("Ali")
+
+
+def f():
+    def b():
+        def h():
+            print("Ali")
+
+
+# Input and Output in Python
+
 
---
+++ Input_and_Output.py
@@ -0,0 +1,15 @@
+x = 2
+y = 8
+
+print(8)
+print(2 + 6)
+print(2, 6, 8)
+print(2, 6, 8, "Ali", sep=" and ")
+print("Area", x * y, sep=": ")
+print(8, end=" print tamam shod.")
+
+print(10, '\n', 9)
+print("masahat:", x * y)
+
+x = input("please Enter your name: ")
+print("your name is: ", x)
