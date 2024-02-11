ce475eeea18c6478a7823593c339898d78b9c664
PersianXM <ali.rashedi@gmail.com>
2024-02-11 21:15:28 +0330

    input and output new file

---
+++ Comparsion Operators.py
@@ -0,0 +1,20 @@
+x = 4
+y = 5
+
+# Equal
+print(x == 4)
+
+# Not equal
+print(y != 4)
+
+# Greater than
+print(x > y)
+
+# Less than
+print(x < y)
+
+# Greater than or equal to
+print(x >= y)
+
+# Less than or equal to
+print(x <= 8)
