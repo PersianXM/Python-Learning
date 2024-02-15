5a0bbcc06ea6dd013c510fb0fb79799388c4b765
PersianXM <ali.rashedi@gmail.com>
2024-02-15 17:40:39 +0330

    input and output new file

---
+++ Expression.py
@@ -0,0 +1,16 @@
+# x + 7 * 9 - 2
+
+# Simple Command
+# Import
+# Pass
+
+def   f():
+      print('reza')
+
+if True:
+  print()
+  elif False:
+      pass
+  else:
+      pass
+`
---
+++ Walrus Operators.py
@@ -0,0 +1,8 @@
+# Walrus Operator :=
+
+
+x = 2
+# print(x == 2)
+print(x := 2)
+print((x := 2) < 5)
+
