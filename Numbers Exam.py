61892c18a85168ef201e24c170f55b5d08c85d09
ARashedi <ali.rashedi@gmail.com>
2024-02-18 14:22:27 +0330

    Commit

--- Numbers Exam.py
+++ Numbers Exam.py
@@ -1,4 +1,6 @@
 import math
+import statistics
+from math import pow
 
 radius = float(input("Enter radius of circle: "))
 
@@ -7,7 +9,30 @@ circumference = 2 * math.pi * radius
 
 print(f"area of circle with {radius} is {area}, and the circumference is {circumference}")
 
+# --------------------------------------------
+
 x = int(input("Enter a number: "))
+
 power2 = math.pow(x, 2)
 power3 = math.pow(x, 3)
-print(f"The power of {x} is {power2}, and the power3, {x} is {power3}")
+
+print(f"The power of {x} is {power2}, and the power3, is {power3}")
+
+# --------------------------------------------
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+power = math.pow(x, y)
+
+print(f"power of {x} bace on {y} is {power}")
+
+# --------------------------------------------
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = int(input("Enter third number: "))
+
+meaning = statistics.mean((x, y, z))
+print(f"The mean of {x} and {y} and {z} is {mean}")
+
